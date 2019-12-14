81b18bbf15f19fc4a3f131cd8b15b2da0fcd69d3
Marcus Paulo <marcusantoniopaulo@gmail.com>
2019-12-14 17:30:09 -0300

    Concluindo os campos do formulário de validação

--- src/containers/Page/CadastroEA/index.js
+++ src/containers/Page/CadastroEA/index.js
@@ -52,23 +52,58 @@ class CalculatorComponent extends React.Component {
   constructor(props) {
      super(props);
 
-     this.handleColaboradorChange = this.handleColaboradorChange.bind(this);
-     this.handleInicioaChange = this.handleInicioaChange.bind(this);
-     this.handleTermoaChange = this.handleTermoaChange.bind(this);
-     this.handleObsChange = this.handleObsChange.bind(this);
+     this.handleColaboradorChange            = this.handleColaboradorChange.bind(this);
+     this.handleInicioaChange                = this.handleInicioaChange.bind(this);
+     this.handleTermoaChange                 = this.handleTermoaChange.bind(this);
+     this.handleObsChange                    = this.handleObsChange.bind(this);
+     this.handleGestorChange                 = this.handleGestorChange.bind(this);
+     this.handleApontamentocontrescalaChange = this.handleApontamentocontrescalaChange.bind(this);
+     this.handleCargoprestservChange         = this.handleCargoprestservChange.bind(this);
+     this.handleRestricaoChange              = this.handleRestricaoChange.bind(this);
+     this.handlePrefiltroChange              = this.handlePrefiltroChange.bind(this);
+     this.handleTiporestricaoChange          = this.handleTiporestricaoChange.bind(this);
+     this.handleCadastroplantaoChange        = this.handleCadastroplantaoChange.bind(this);
+     this.handleTotalcadastroplantaoChange   = this.handleTotalcadastroplantaoChange.bind(this);
+     this.handleEcolaboradorChange           = this.handleEcolaboradorChange.bind(this);
+     this.handleCpfChange                    = this.handleCpfChange.bind(this);
+     
      
      this.handleSubmit = this.handleSubmit.bind(this);
      this.state = {
                    colaborador: '',
                    inicioa:'',
                    termoa:'',
-                   obs:''
+                   obs:'',
+                   gestor:'',
+                   apontamentocontrescala:'',
+                   cargoprestserv:'',
+                   restricao:'',
+                   prefiltro:'',
+                   tiporestricao:'',
+                   cadastroplantao:'',
+                   totalcadastroplantao:'',
+                   ecolaborador:'',
+                   cpf:''
                   };
   
   }
   
   handleSubmit(event) {
-    alert('Colaborador: ' + this.state.colaborador+'Inicio Apontamento'+this.state.inicioa+'Termo Apontamento'+this.state.termoa+'Obs'+this.state.obs);
+    alert('Colaborador:           '+ this.state.colaborador+
+          'Inicio Apontamento     '+this.state.inicioa+
+          'Termo Apontamento      '+this.state.termoa+
+          'Obs                    '+this.state.obs+
+          'Gestor                 '+this.state.gestor+
+          'apontamentocontrescala:'+this.state.apontamentocontrescala+
+          'cargoprestserv:        '+this.state.cargoprestserv+
+          'restricao:             '+this.state.restricao+
+          'prefiltro:             '+this.state.prefiltro+
+          'tiporestricao:         '+this.state.tiporestricao+
+          'cadastroplantao:       '+this.state.cadastroplantao+
+          'totalcadastroplantao:  '+this.state.totalcadastroplantao+
+          'ecolaborador:          '+this.state.ecolaborador+
+          'cpf:                   '+this.state.cpf
+        );
     event.preventDefault();
   }
  
@@ -83,13 +118,41 @@ class CalculatorComponent extends React.Component {
  this.setState ({termoa: event.target.value}); 
  }
   handleObsChange (event ){ 
- this.setState ({obs: event.target.value}); 
+  this.setState ({obs: event.target.value}); 
  }
  
+ handleGestorChange (event ){ 
+  this.setState ({gestor: event.target.value}); 
+ }
+ handleApontamentocontrescalaChange (event ){ 
+  this.setState ({apontamentocontrescala: event.target.value}); 
+ }
+ handleCargoprestservChange (event ){ 
+  this.setState ({cargoprestserv: event.target.value}); 
+ }
+ handleRestricaoChange (event ){ 
+  this.setState ({restricao: event.target.value}); 
+ }
+  handlePrefiltroChange (event ){ 
+  this.setState ({prefiltro: event.target.value}); 
+ }
+  handleTiporestricaoChange (event ){ 
+  this.setState ({tiporestricao: event.target.value}); 
+ }
+  handleCadastroplantaoChange (event ){ 
+  this.setState ({cadastroplantao: event.target.value}); 
+ }
+ handleTotalcadastroplantaoChange (event ){ 
+  this.setState ({totalcadastroplantao: event.target.value}); 
+ }
+  handleEcolaboradorChange (event ){ 
+  this.setState ({ecolaborador: event.target.value}); 
+ }
  
+ handleCpfChange (event ){ 
+  this.setState ({cpf: event.target.value}); 
+ }
  
- 
-
   render() {
     return (
       <>
@@ -109,7 +172,7 @@ class CalculatorComponent extends React.Component {
                                 <tr>
                                  <td>
                                    <div style={{ marginBottom: 16 , width:500}}>
-                                   Colaborador *<Input type="text" name="colaborador" id="nome" value = {this.state.colaborador} onChange = {this.handleColaboradorChange}/>
+                                   Colaborador *<Input type="text" name="colaborador" id="nome" value = {this.state.colaborador} onChange = {this.handleColaboradorChange} required="require"/>
                                    
                                    </div>
                                  </td>
@@ -127,41 +190,38 @@ class CalculatorComponent extends React.Component {
                                  <td>
                                    <div style={{ marginBottom: 16 , width:250}}>
                                      <img src='./images/calendario.png'  width='22' height='22'/>
-                                      &nbsp;Inicio de Apontamento *<Input type="date" name="inicioa" id="inicioa" value = {this.state.inicioa} onChange = {this.handleInicioaChange}/>
-                   
+                                      &nbsp;Inicio de Apontamento *<Input type="date" name="inicioa" id="inicioa" value = {this.state.inicioa} onChange = {this.handleInicioaChange} required="require"/>
                                     </div>
                                   </td>
                                   <td>
                                   <div style={{ marginBottom: 16 , width:250}}>
                                   <img src='./images/calendario.png'  width='22' height='22'/>
-                                  &nbsp;Termo de Apontamento *<Input type="date" name="termoa" id="termoa" value = {this.state.termoa} onChange = {this.handleTermoaChange}/>
+                                  &nbsp;Termo de Apontamento *<Input type="date" name="termoa" id="termoa" value = {this.state.termoa} onChange = {this.handleTermoaChange} required="require"/>
                                   </div>
                                  </td>
                                   </tr>
                                 </table>
                               </div>
                               <div style={{ marginBottom: 16 , width:500}}>
-                             
                                 OBS <br /><Input type="text" name="obs" id="obs" value = {this.state.obs} onChange = {this.handleObsChange}/>
-                                 
                               </div>
                               <div>
                                 <table>
                                        <tr>
                                         <td>
                                           <div style={{ marginBottom: 16 , width:400}}><br />
-                                            Gestor  *<Input type="text" />
+                                            Gestor *<Input type="text" name="gestor" id="gestor" value = {this.state.gestor} onChange = {this.handleGestorChange} required="require"/>
                                             Usuário que receberá notificações caso o apontamento seja incompleto
                                            </div>
                                         </td>
                                         <td>
                                            <div style={{ marginBottom: 16 , width:700}}><br />
                                              Apontamento controlado por escala? <br /> 
-                                             <select defaultValue="Não" style={{ width: 225, height:30}}>
+                                             <select defaultValue="Não" style={{ width: 225, height:30}} name="apontamentocontrescala" id="apontamentocontrescala" value = {this.state.apontamentocontrescala} onChange = {this.handleApontamentocontrescalaChange} >
                                               <option value="Não">Não</option>
                                               <option value="Sim">Sim</option>
                                              </select><br />
-                                             Marcar Não apenas para áreas que não prestam serviços <br />para Projeto/Contrato
+                                             Marcar Não apenas para áreas que não prestam serviços <br />para Projeto| Contrato
                                            </div>
                                         </td>
                                        </tr>
@@ -173,14 +233,15 @@ class CalculatorComponent extends React.Component {
                                        <tr>
                                         <td>
                                            <div style={{ marginBottom: 16, width:400 }}><br />
-                                            Cargo prestação de serviço  *<Input type="text" />
+                                            Cargo prestação de serviço
+                                            *<Input type="text" name="cargoprestserv" id="cargoprestserv" value = {this.state.cargoprestserv} onChange = {this.handleCargoprestservChange} required="require"/>
                                             Preencher somente se colaborador <br />presta serviços para Cliente - Essa informação
                                            </div>
                                         </td>
                                         <td>
                                            <div style={{ marginBottom: 16 , width:500}}><br />
                                              Possuei Restrição? <br />
-                                              <select defaultValue="Não" style={{ width: 225, height:30}}>
+                                              <select defaultValue="Não" style={{ width: 225, height:30}} name="restricao" id="restricao" value = {this.state.restricao} onChange = {this.handleRestricaoChange} >
                                                 <option value="Não">Não</option>
                                                 <option value="Sim">Sim</option>
                                               </select><br /> 
@@ -197,13 +258,14 @@ class CalculatorComponent extends React.Component {
                                        <tr>
                                         <td>
                                           <div style={{ marginBottom: 16 , width:400}}>
-                                            Pré-Filtro  *<Input type="text" />
+                                            Pré-Filtro  *<Input type="text" name="prefiltro" id="prefiltro" value = {this.state.prefiltro} onChange = {this.handlePrefiltroChange} required="require"/>
+                                            
                                            </div>
                                         </td>
                                         <td>
                                            <div style={{ marginBottom: 16 , width:500}}><br /><br />
                                              Tipo de Restrição <br />
-                                              <select defaultValue="Nenhuma:" style={{ width: 225, height:30}}>
+                                              <select defaultValue="Nenhuma:" style={{ width: 225, height:30}} name="tiporestricao" id="tiporestricao" value = {this.state.tiporestricao} onChange = {this.handleTiporestricaoChange} >
                                                <option value="Nenhuma">Nenhuma</option>
                                                <option value="Sim">Sim</option>
                                               </select><br />
@@ -219,8 +281,7 @@ class CalculatorComponent extends React.Component {
                                   <tr>
                                   
                                    <td>
-                                     <select id="s" multiple style={{ width: 225, height:100}}>
-                                        
+                                     <select multiple style={{ width: 225, height:30}} name="cadastroplantao" id="cadastroplantao" value = {this.state.cadastroplantao} onChange = {this.handleCadastroplantaoChange} >
                                        <option value="a">Poo de Recursos-For</option>
                                        <option value="b">Poo de Recursos-Rec</option>
                                        <option value="c">Poo de Recursos-Salv</option>
@@ -232,7 +293,7 @@ class CalculatorComponent extends React.Component {
                                      &nbsp;&nbsp;<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; listbox_moveacross('d', 's')" data-cf-modified-13ce572d586f6e3b4267b954-=""><img src='./images/retornar.png'  width='22' height='22'/></a>&nbsp;
                                    </td>
                                    <td>
-                                    <select id="d" size="10" multiple style={{ width: 225, height:100}} >
+                                    <select multiple style={{ width: 225, height:30}} name="totalcadastroplantao" id="totalcadastroplantao" value = {this.state.totalcadastroplantao} onChange = {this.handleTotalcadastroplantaoChange} >
                                       <option value="a">Outros</option>
                                     </select>
                                    </td>
@@ -249,7 +310,7 @@ class CalculatorComponent extends React.Component {
                                         <td>
                                           <div style={{ marginBottom: 16}}>
                                            É colaborador <br />
-                                            <select  value={this.state.favoriteColor2} onChange={this.handleColorSelect2} style={{ width: 211, height:30}}>
+                                             <select  style={{ width: 211, height:30}}  name="ecolaborador" id="ecolaborador" value = {this.state.ecolaborador} onChange = {this.handleEcolaboradorChange} >
                                              <option value="s">Sim</option>
                                              <option value="n">Não</option>
                                            </select>
@@ -258,7 +319,7 @@ class CalculatorComponent extends React.Component {
                                         <td>
                                            <div style={{ marginBottom: 16 , width:200}}>
                                              CPF
-                                             <Input type="text" />
+                                              <Input type="text" name="cpf" id="cpf" value = {this.state.cpf} onChange = {this.handleCpfChange} />
                                            </div>
                                         </td>
                                        </tr>
